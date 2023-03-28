77f525c0ab1e41c939693d52ce38feb5349dc965
tozmart <tozmart@tozmart.local>
2023-03-28 11:16:00 +0800

    使用次数修改为5次

--- service/user_service.go
+++ service/user_service.go
@@ -120,8 +120,8 @@ func generateUser(c *gin.Context) (int, entity.Response) {
 			Language:         req.Language,
 			NickName:         req.NickName,
 			Province:         req.Province,
-			RemainUsageCount: 10,
-			TotalUsageCount:  10,
+			RemainUsageCount: 5,
+			TotalUsageCount:  5,
 			CreatedAt:        time.Now(),
 			UpdatedAt:        time.Now(),
 		}
